554e468981d7cb2ccd5ddb842b156e91a9823983
jiomavj-tech <jiomavj@gmail.com>
2026-09-21 19:23:00 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME="academia-classica-v98";
+const CACHE_NAME="academia-classica-v97";
 const APP_SHELL=[
   "./","./index.html","./manifest.webmanifest","./version.json",
   "./icon-192.png","./icon-512.png","./icon-maskable-512.png"
